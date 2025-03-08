145edbec7b83e7b058bdbbffc31d02aeae54c82c
Vini-19 <marcosanchez-1@outlook.com>
2025-03-07 23:04:08 -0600

    subir

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import {defineConfig} from 'vite'
 
 export default defineConfig({
     appType: 'mpa',
-    base: "https://vini-19.githubio.vini-19/",
+    base: "https://vini-19.github.io/vini-19/",
 });
