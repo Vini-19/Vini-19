cca4bd663b9b5b74967ed5b2d92e181f660e6c3d
Vini-19 <marcosanchez-1@outlook.com>
2025-12-07 23:06:32 -0600

    agrego load de carga

---
+++ js/load.js
@@ -0,0 +1,10 @@
+// Tiempo que quieres que el loader permanezca visible
+const timeout = 500; // 2 segundos
+
+// Espera a que toda la página cargue
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        const loader = document.getElementById("loader-overlay");
+        loader.classList.add("hidden");
+    }, timeout);
+});
