fff1e82f84f460b55da99ce04327f99912370351
Vini-19 <marcosanchez-1@outlook.com>
2025-12-07 22:47:21 -0600

    mas informacion y una animacion modificada en el nav para cambio de color al scroll

--- js/nav_scroll.js
+++ js/nav_scroll.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const nav = document.querySelector(".navbar");
 
     window.addEventListener("scroll", function () {
-        if (window.scrollY > 0.5) {
+        if (window.scrollY > 0.2) {
             nav.classList.add("scrolled");
         } else {
             nav.classList.remove("scrolled");
